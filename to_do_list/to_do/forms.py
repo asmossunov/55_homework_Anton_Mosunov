c0c41b314055505cf362f7539f3e380933252556
Mosunov Anton <antonmossunov@gmail.com>
2022-09-29 23:19:13 +0600

    Создание частичного шаблона для отображения формы
    Создан частичный шаблон для формы task_form_partial.html
    Создан файл forms.py с классом формы и заполнен полями для заполнения и валидации

---
+++ to_do_list/to_do/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import widgets
+
+from to_do.choice_config import CHOICES
+
+
+class TaskForm(forms.Form):
+    task_text = forms.CharField(max_length=200, required=True, label='Название задачи',
+                                widget=forms.TextInput({'class': 'form-input'}))
+    state = forms.ChoiceField(choices=CHOICES, label='Состояние')
+    task_description = forms.CharField(max_length=2000, required=True, label='Описание',
+                                       widget=widgets.Textarea)
+    deadline = forms.DateField(required=False, widget=widgets.SelectDateWidget, label='Дедлайн')
+
+    def clean_task_text(self):
+        task_text = self.cleaned_data.get('task_text')
+        if len(task_text) < 2:
+            raise ValidationError('Поле должно быть заполнено более чем одним символом')
+        return task_text

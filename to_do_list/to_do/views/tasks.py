a4720ae4039f3870f4a094043b06935eeb7d11ff
Mosunov Anton <antonmossunov@gmail.com>
2022-09-29 23:28:55 +0600

    Рефакторинг функционала представлений создания и редактирования задач
    Изменена реализация вышеперечисленных представлений в связи с
    появлением объекта формы
    Удалён ставший бесполезным функционал

--- to_do_list/to_do/views/tasks.py
+++ to_do_list/to_do/views/tasks.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 from to_do.models import Task
 from to_do.choice_config import CHOICES
+from to_do.forms import TaskForm
 
 
 def index_view(request):
@@ -16,31 +17,18 @@ def index_view(request):
         return render(request, 'index.html', context)
 
 def add_task_view(request):
+    form = TaskForm()
     if request.method == 'GET':
+        context = {'form': form}
+        return render(request, 'add_task.html', context)
+    form = TaskForm(request.POST)
+    if not form.is_valid():
         context = {
-            'CHOICES': CHOICES
+            'form': form
         }
         return render(request, 'add_task.html', context)
-    return added_task_prepare(request)
-
-def added_task_prepare(request):
-    if request.POST.get("task_text") == '':
-        task_text = 'тема не определена'
-    else:
-        task_text = request.POST.get("task_text")
-    if request.POST.get("deadline") == '':
-        deadline = None
-    else:
-        deadline = request.POST.get("deadline")
-    task = Task.objects.create(
-        task_text=task_text,
-        task_description=request.POST.get("task_description"),
-        state=request.POST.get("state"),
-        deadline=deadline
-    )
-    print(f'Добавлена запись: {task}')
-    return redirect(reverse('task_detail', kwargs={'pk': task.pk}) )
-
+    task = Task.objects.create(**form.cleaned_data)
+    return redirect('task_detail', pk=task.pk)
 
 def task_view(request, pk):
     if request.POST.get("task_text") == '':
@@ -66,17 +54,27 @@ def task_view(request, pk):
     return render(request, 'task.html', context={'task': task, 'CHOICES': CHOICES})
 
 def task_edit_view(request, pk):
-    task = Task.objects.get(pk=pk)
-    context = {
-            'CHOICES': CHOICES,
-            'task': task,
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'GET':
+        form = TaskForm(initial={
+            'task_text': task.task_text,
+            'state': task.state,
+            'task_description': task.task_description,
+            'deadline': task.deadline
+        })
+        return render(request, 'edit_task.html', context={'form': form, 'task': task})
+    form = TaskForm(request.POST)
+    if not form.is_valid():
+        context = {
+            'choices': CHOICES,
+            'form': form
         }
-    return render(request, 'edit_task.html', context)
+        return render(request, 'add_task.html', context)
+    task = Task.objects.create(**form.cleaned_data)
 
 
 def delete_view(request, pk):
     task = get_object_or_404(Task, pk=pk)
-    print(pk)
     context = {
         'task': task
     }
